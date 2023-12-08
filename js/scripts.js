14df290c342664d35c33fba2da7a07eaba25001c
Eugene Kudelya <kudel89@gmail.com>
2023-12-08 13:34:35 +0200

    Some changes to the Shipping Service and other

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger')
-const navMenu = document.querySelector('.nav-menu')
+const topnav = document.querySelector('.topnav')
 const navLink = document.querySelectorAll('.nav-link')
 
 hamburger.addEventListener('click', mobileMenu)
@@ -7,10 +7,10 @@ navLink.forEach((n) => n.addEventListener('click', closeMenu))
 
 function mobileMenu() {
   hamburger.classList.toggle('active')
-  navMenu.classList.toggle('active')
+  topnav.classList.toggle('active')
 }
 
 function closeMenu() {
   hamburger.classList.remove('active')
-  navMenu.classList.remove('active')
+  topnav.classList.remove('active')
 }
