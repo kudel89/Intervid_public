58f6d6d7f58b91d5aea0b94a1736822145157dec
Eugene Kudelya <kudel89@gmail.com>
2023-12-10 11:58:38 +0200

    Big layout fixes for Services and added all service pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,59 @@
-const hamburger = document.querySelector('.hamburger')
-const topnav = document.querySelector('.topnav')
-const navLink = document.querySelectorAll('.nav-link')
+fetch('./header.html')
+  .then((response) => {
+    return response.text()
+  })
+  .then((data) => {
+    /* Inner header */
+    document.querySelector('header').innerHTML = data
 
-hamburger.addEventListener('click', mobileMenu)
-navLink.forEach((n) => n.addEventListener('click', closeMenu))
+    /* Responsive navbar MENU */
+    const hamburger = document.querySelector('.hamburger')
+    const topnav = document.querySelector('.topnav')
+    const navLink = document.querySelectorAll('.nav-link')
 
-function mobileMenu() {
-  hamburger.classList.toggle('active')
-  topnav.classList.toggle('active')
-}
+    hamburger.addEventListener('click', mobileMenu)
+    navLink.forEach((n) => n.addEventListener('click', closeMenu))
 
-function closeMenu() {
-  hamburger.classList.remove('active')
-  topnav.classList.remove('active')
-}
+    function mobileMenu() {
+      hamburger.classList.toggle('active')
+      topnav.classList.toggle('active')
+    }
+
+    function closeMenu() {
+      hamburger.classList.remove('active')
+      topnav.classList.remove('active')
+    }
+  })
+
+fetch('./footer.html')
+  .then((response) => {
+    return response.text()
+  })
+  .then((data) => {
+    /* Inner header */
+    document.querySelector('footer').innerHTML = data
+  })
+
+fetch('./section-contact-us.html')
+  .then((response) => {
+    return response.text()
+  })
+  .then((data) => {
+    /* Inner 'Contact us' section */
+    const element = document.querySelector('.contactus-section')
+    if (!!element) {
+      return (element.innerHTML = data)
+    }
+  })
+
+fetch('./section-more-for-you.html')
+  .then((response) => {
+    return response.text()
+  })
+  .then((data) => {
+    /* Inner 'More for you' section */
+    const element = document.querySelector('.more-for-you')
+    if (!!element) {
+      return (element.innerHTML = data)
+    }
+  })
